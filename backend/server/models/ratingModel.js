5057be4bfe47ddcc90091d1d742efcd6bdd94309
xiomary <Xiomary331@gmail.com>
2024-03-18 23:20:37 -0400

    added recipeId field to model

--- backend/server/models/ratingModel.js
+++ backend/server/models/ratingModel.js
@@ -3,17 +3,24 @@ const mongoose = require("mongoose");
 const ratingSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: true, // Ensuring username is always provided
+    label: "username",
   },
+
+  recipeId:{
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref:'Recipe',
+  },
+  
   score: {
     type: Number,
-    required: true,
+    required: true, // Corrected to ensure a score is always provided
     min: 1,
     max: 5,
   },
+}, {
+  collection: "ratings" // Corrected syntax here
 });
 
-
-const Rating = mongoose.model("Rating", ratingSchema);
-
-module.exports = Rating;
+module.exports = mongoose.model('ratings', ratingSchema);
