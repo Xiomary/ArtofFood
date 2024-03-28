9123e9b939885ffabf4a1482fc1bb38ec84673c0
xiomary <Xiomary331@gmail.com>
2024-03-28 14:43:46 -0400

    updated rating model

--- backend/server/models/ratingModel.js
+++ backend/server/models/ratingModel.js
@@ -1,26 +1,32 @@
 const mongoose = require("mongoose");
 
 const ratingSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true, // Ensuring username is always provided
-    label: "username",
-  },
-
-  recipeId:{
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
     required: true,
-    ref:'Recipe',
   },
-  
+  recipeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Recipe',
+    required: true,
+    // Remove unique: true from here
+  },
   score: {
     type: Number,
-    required: true, // Corrected to ensure a score is always provided
+    required: true,
     min: 1,
     max: 5,
   },
 }, {
-  collection: "ratings" // Corrected syntax here
+  collection: "ratings",
+  // Add timestamps or any other options needed
 });
 
-module.exports = mongoose.model('ratings', ratingSchema);
+// Define a compound unique index on userId and recipeId
+ratingSchema.index({ userId: 1, recipeId: 1 }, { unique: true });
+
+module.exports = mongoose.model('Rating', ratingSchema);
+
+
+
