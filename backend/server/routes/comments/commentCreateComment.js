11f18502d1833332c5a4e68d4c94a7c7d89f99a5
xiomary <Xiomary331@gmail.com>
2024-04-05 21:41:20 -0400

    create comment route

---
+++ backend/server/routes/comments/commentCreateComment.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const Comment = require("../../models/commentModel"); // Make sure the path is correct
+
+// POST /comment/addComment/:id (where :id is the recipeId)
+router.post("/addComment/:id", async (req, res) => {
+  const { content } = req.body; 
+  const { userId } = req.body; 
+
+  const recipeId = req.params.id;
+
+  const newComment = new Comment({
+    userId,
+    recipeId,
+    content,
+  });
+
+  try {
+    const savedComment = await newComment.save();
+    res.status(201).send(savedComment);
+  } catch (error) {
+    res
+      .status(400)
+      .send({
+        message: "Error trying to create comment",
+        error: error.message,
+      });
+  }
+});
+
+module.exports = router;
