6567c27618fdc77af86450a0368c34277549b09b
xiomary <Xiomary331@gmail.com>
2024-04-19 19:19:48 -0400

    added more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,56 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
-import LandingPage from "./components/pages/landingPage";
+import LandingPage from "./components/pages/LandingPage";
 import HomePage from "./components/pages/HomePage";
 import Login from "./components/pages/LoginPage";
 import Signup from "./components/pages/RegisterPage";
-import PrivateUserProfile from "./components/pages/privateUserProfilePage";
+import PrivateUserProfile from "./components/pages/PrivateUserProfilePage";
 import RecipeForm from "./components/pages/RecipeForm";
 import RecipeList from "./components/pages/RecipeList";
 import RecipeDetails from "./components/pages/RecipeDetails";
-import { createContext } from "react";
 import getUserInfo from "./utilities/decodeJwt";
 import SearchComponent from "./components/pages/SearchComponent";
-//import star rating
 import StarRating from "./components/pages/StarRatings";
 import UserRecipes from "./components/pages/UserRecipes";
-//import recipe edit form
 import RecipeEditForm from "./components/pages/RecipeEditForm";
 import RecipeCommentForm from "./components/pages/RecipeCommentForm";
-export const UserContext = createContext();
+import ProfileCreate from "./components/pages/ProfileCreate";
+import ProfileDetailsPage from "./components/pages/ProfileDetailspage";
+import ProfileUpdate from "./components/pages/ProfileUpdate";
+//admin delete
+import AdminDelete from "./components/pages/AdminDeleteUsers";
 
 const App = () => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    setUser(getUserInfo());
+    const userData = getUserInfo();
+    setUser(userData);
+    setIsAdmin(userData?.isAdmin || false); 
   }, []);
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/homePage" element={<HomePage />} />
-        <Route exact path="/loginPage" element={<Login />} />
-        <Route exact path="/privateUserProfile" element={<PrivateUserProfile />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/recipeForm" element={<RecipeForm />} />
-        <Route exact path="/recipeList" element={<RecipeList />} />
-        <Route exact path="/recipeDetails/:id" element={<RecipeDetails />} />
-        <Route exact path="/searchComponent" element={<SearchComponent />} />
-        <Route exact path="/ratingComponent/:id" element={<StarRating />} />
-        <Route exact path="/recipeEdit/:id" element={<RecipeEditForm />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/privateUserProfile" element={<PrivateUserProfile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/loginPage" element={<Login />} />
+        <Route path="/recipeForm" element={<RecipeForm />} />
+        <Route path="/recipeList" element={<RecipeList />} />
+        <Route path="/recipeDetails/:id" element={<RecipeDetails />} />
+        <Route path="/searchComponent" element={<SearchComponent />} />
+        <Route path="/ratingComponent/:id" element={<StarRating />} />
+        <Route path="/recipeEdit/:id" element={<RecipeEditForm />} />
         <Route path="/addComment/:id" element={<RecipeCommentForm />} />
-
-        
+        <Route path="/profileCreate" element={<ProfileCreate />} />
+        <Route path="/profileDetails/:userId" element={<ProfileDetailsPage />} />
+        <Route path="/profileUpdate/:userId" element={<ProfileUpdate />} />
+        <Route path="/adminDelete" element={<AdminDelete />} />
       </Routes>
     </>
   );
