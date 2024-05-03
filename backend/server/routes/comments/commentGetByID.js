53daeee7edc8847cf87635ea73d1abd6c12cd340
xiomary <Xiomary331@gmail.com>
2024-05-02 20:40:03 -0400

    updated comment route

--- backend/server/routes/comments/commentGetByID.js
+++ backend/server/routes/comments/commentGetByID.js
@@ -16,25 +16,5 @@ router.get("/:recipeId/comments", async (req, res) => {
   }
 });
 
-router.post("/addComment/:id", async (req, res) => {
-  const { content, userId } = req.body;
-  const recipeId = req.params.id;
-
-  const newComment = new Comment({
-    userId,
-    recipeId,
-    content,
-  });
-
-  try {
-    const savedComment = await newComment.save();
-    res.status(201).send(savedComment);
-  } catch (error) {
-    res.status(400).send({
-      message: "Error trying to create comment",
-      error: error.message,
-    });
-  }
-});
 
 module.exports = router;
